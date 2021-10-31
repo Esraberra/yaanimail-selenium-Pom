e176358dea812f9bd7616ebcb1b6e31fcbdff4d2
Esraberra <esraberra6@gmail.com>
2021-10-31 11:34:11 +0100

    first commit

---
+++ src/test/java/pages/InboxClass.java
@@ -0,0 +1,24 @@
+package pages;
+
+public class InboxClass
+    /*
+    WebDriver'a bağlan
+
+Login sayfasına git(https://www.yaanimail.com/login)
+
+Gelen ekranda kullanıcı adını gir (xxxx)
+
+Kullanıcı şifresini gir (yyyy)
+
+Giriş yap butonuna bas
+
+Inbox sayfasında sol tarafta bulunan Yeni E-Posta elementinin varlığını kontrol et
+     */
+
+{
+
+
+
+
+
+}
